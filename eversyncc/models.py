080c24a4c61977f6d980f5dc74020d8ae643249f
rudy3333 <mmax3163@gmail.com>
2025-05-03 22:01:13 +0200

    init

---
+++ eversyncc/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+    #unsecurrrr
+    def reset_password(self):
+        self.password = "123456"
+        self.save()
