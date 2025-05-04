4c6e84ef188e4fadbdc480fec1d3a8ef5ba7df7f
rudy3333 <mmax3163@gmail.com>
2025-05-04 22:18:19 +0200

    registration

--- eversyncc/views.py
+++ eversyncc/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 from django.contrib.auth import logout
 
@@ -10,8 +11,16 @@ def logout_view(request):
     return redirect('index')
 
 def register(request):
-    return render(request, "register.html")
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
 
 @login_required
 def index(request):
     return render(request, "index.html")
+
