a20096f0fe501fbf8c1c0dfb8d03587076689da1
rudy3333 <mmax3163@gmail.com>
2025-05-05 23:24:33 +0200

    fix

--- eversyncc/views.py
+++ eversyncc/views.py
@@ -6,10 +6,10 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm, Password
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.contrib.auth.views import PasswordChangeView
-from .forms import UsernameChangeForm
+from .forms import UsernameChangeForm, EventForm
 from django.contrib import messages
 from django.contrib.auth.views import LoginView
-
+from.models import Event
 
 
 def logout_view(request):
@@ -47,7 +47,7 @@ def change_username(request):
             new_username = form.cleaned_data['new_username']
             user = request.user
             user.username = new_username
-            form.save()
+            user.save()
             messages.success(request, "Username updated successfully!")
             return redirect('manage')  # Redirect back to the manage account page
         else:
@@ -55,7 +55,8 @@ def change_username(request):
     else:
         form = UsernameChangeForm()
 
-    return render(request, 'manage.html', {'username_form': form})
+    return render(request, 'manage.html', {'form': form})
+
 
 
 
@@ -79,3 +80,14 @@ class RedirectFromLogin(LoginView):
         if request.user.is_authenticated:
             return redirect('/')  # Change 'home' to your desired redirect
         return super().dispatch(request, *args, **kwargs)
+    
+def event_list(request):
+    events = Event.objects.filter(user=request.user)
+
+def create_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.user = request.user
+            event.save()
