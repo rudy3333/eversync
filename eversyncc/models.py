b36b34a4ce45299c626b43c3627f77914fedefab
rudy3333 <mmax3163@gmail.com>
2025-05-06 00:49:59 +0200

    fixed to send to root

--- eversyncc/models.py
+++ eversyncc/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Document(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    file = models.FileField(upload_to="files/files")
+    file = models.FileField(upload_to="")
 
     def __str__(self):
         return self.title
