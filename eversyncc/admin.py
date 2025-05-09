ccf7480242b8a3679a9b50c47b007988455ca7be
rudy3333 <mmax3163@gmail.com>
2025-05-10 00:21:41 +0200

    early notes wip, functioning but some styling is off

--- eversyncc/admin.py
+++ eversyncc/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Document, Event
+from .models import Document, Event, Notes
 
 # Register your models here.
 
 admin.site.register(Document)
 admin.site.register(Event)
+admin.site.register(Notes)
