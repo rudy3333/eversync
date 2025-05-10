5912d44fd4f6673a28a974fec084a020d7d986ac
rudy3333 <mmax3163@gmail.com>
2025-05-10 19:10:41 +0200

    update settings for PostgreSQL

--- eversync/settings.py
+++ eversync/settings.py
@@ -16,6 +16,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+APP_NAME = os.environ.get("FLY_APP_NAME")
+ALLOWED_HOSTS = [".fly.dev", "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev"] 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -92,11 +96,14 @@ WSGI_APPLICATION = 'eversync.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'), # database name
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
